224cfa08ba57a158ba3377715458fbe44e244b04
fushang318 <fushang318@gmail.com>
2015-01-16 17:27:24 +0800

    add post

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/weixin/verify' => 'weixin#verify'
+  post '/weixin/verify' => 'weixin#verify'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
